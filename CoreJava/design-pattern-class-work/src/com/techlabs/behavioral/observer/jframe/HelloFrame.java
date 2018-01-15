2de24d7bfa7cbc6233ee0839f1df3c34d660f08d
Brijesh <brijeshyadav152@outlook.com>
2018-01-15 11:32:04 +0530

    Added Worked till 13/01/2018

--- CoreJava/design-pattern-class-work/src/com/techlabs/behavioral/observer/jframe/HelloFrame.java
+++ CoreJava/design-pattern-class-work/src/com/techlabs/behavioral/observer/jframe/HelloFrame.java
@@ -3,23 +3,27 @@ package com.techlabs.behavioral.observer.jframe;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 
-public class Frame {
-	public static void main(String[] args) {
+public class HelloFrame extends JFrame {
+	private JToggleButton loginButton;
+	private JPanel helloPanel;
 
-		JFrame helloFrame = new JFrame("Hello Jframe");
-		helloFrame.setSize(350, 200);
-		helloFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public HelloFrame() {
+		loginButton = new JToggleButton("Hello");
+		helloPanel = new JPanel();
+		start();
+	}
 
-		JPanel helloPanel = new JPanel();
-		helloFrame.add(helloPanel);
+	public void start() {
 
-		JToggleButton loginButton = new JToggleButton("Hello");
+		this.setSize(350, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.add(helloPanel);
+
 		loginButton.setBounds(10, 80, 80, 25);
 		loginButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -33,7 +37,7 @@ public class Frame {
 		});
 		helloPanel.add(loginButton);
 
-		helloFrame.setVisible(true);
+		this.setVisible(true);
 	}
 
 	private static void sayHello() {
---
+++ CoreJava/design-pattern-class-work/src/com/techlabs/behavioral/observer/jframe/test/TestHelloFrame.java
@@ -0,0 +1,18 @@
+package com.techlabs.behavioral.observer.jframe.test;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+
+import com.techlabs.behavioral.observer.jframe.HelloFrame;
+
+public class TestHelloFrame {
+	public static void main(String[] args) {
+		new HelloFrame();
+	}
+}
