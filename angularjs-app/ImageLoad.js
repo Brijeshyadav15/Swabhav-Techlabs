aa7042157c10242fcf65f868ae79d9a171685442
Brijesh <brijeshyadav152@outlook.com>
2018-01-24 18:12:55 +0530

    Added work till 24/01/2018

---
+++ angularjs-app/ImageLoad.js
@@ -0,0 +1,25 @@
+angular.module('imageLoad', []).controller('imageLoadCtrl', function ($scope, $scope) {
+
+    $scope.resume = function () {
+        console.log("Inside Function");
+        $scope.name = "Brijesh";
+        $scope.age = "22";
+        $scope.location = "Mumbai";
+        $scope.image = "abcd.jpg";
+    }
+
+    $scope.resumes = function () {
+        $scope.resumes = [{
+            name: "Brijesh",
+            age: "22",
+            location: "Mumbai",
+            image: "abcd.jpg"
+        },
+        {
+            name: "Akash",
+            age: "21",
+            location: "Mumbai",
+            image: "abc.jpg"
+        }]
+    }
+});
