d89f902728731d6a73a10137d7dfae69dd981f98
Brijesh <brijeshyadav152@outlook.com>
2018-01-30 17:15:27 +0530

    Added DotNET App

---
+++ DotNET/C#/StudentAPP/StudentAPP/Account.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace StudentAPP
+{
+    class Account
+    {
+        private String _name;
+        private int _accountnum;
+        private int _balance;
+        private static readonly int MINIMUM_BALANCE = 500;
+
+        public Account(String name, int accountnum, int balance)
+        {
+            _name = name;
+            _accountnum = accountnum;
+            _balance = balance;
+        }
+
+        public String Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int AccountNum
+        {
+            get
+            {
+                return _accountnum;
+            }
+            set
+            {
+                _accountnum = value;
+            }
+        }
+
+        public int Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (Balance - amount >= MINIMUM_BALANCE)
+            {
+                Balance = Balance - amount;
+                Console.WriteLine("Withdraw Successful. Main Balance is " + Balance);
+            }
+            else
+            {
+                Console.WriteLine("Your Balance is low. Withdraw not completed");
+            }
+        }
+
+        public void Deposit(int amount)
+        {
+            Balance = Balance + amount;
+            Console.WriteLine("Deposit Successful. Main Balance is " + Balance);
+        }
+
+    }
+}
---
+++ DotNET/C#/StudentAPP/StudentAPP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StudentAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account("Brijesh", 1, 4000);
+            Account account2 = new Account("Akash", 2, 5000);
+
+            Console.WriteLine(account2.AccountNum);
+            Console.WriteLine(account2.Name);
+            Console.WriteLine(account2.Balance);
+            account1.Withdraw(6000);
+
+            Console.WriteLine(account1.AccountNum);
+            Console.WriteLine(account1.Name);
+            Console.WriteLine(account1.Balance);
+            account1.Deposit(2000);
+
+
+        }
+    }
+}
