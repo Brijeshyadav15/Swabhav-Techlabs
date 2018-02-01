4debd9cb6177a21a2d2472904f664b0093bc62a8
Brijesh <brijeshyadav152@outlook.com>
2018-02-01 19:14:46 +0530

    Added Custom Attributes getter

---
+++ DotNET/C#/AttributeFinderApp/AttributeFinderApp/Employee.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AttributeFinderApp
+{
+    class Employee
+    {
+        private int _id;
+        private String _name;
+        private String _desg;
+        private String _reportingId;
+        private DateTime _dob;
+        private int _salary;
+        private String _da;
+        private String _department;
+        private List<Employee> _reportees;
+
+        public Employee(int id, String name, String desg, String reportingid,
+            DateTime dob, int salary, String da, String department)
+        {
+            _id = id;
+            _name = name;
+            _desg = desg;
+            _reportingId = reportingid;
+            _dob = dob;
+            _salary = salary;
+            _da = da;
+            _department = department;
+            _reportees = new List<Employee>();
+
+        }
+
+        [NeedRefactoring]
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public String Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public String Desg
+        {
+            get
+            {
+                return _desg;
+            }
+        }
+        public String ReportingId
+        {
+            get
+            {
+                return _reportingId;
+            }
+        }
+        public DateTime Dob
+        {
+            get
+            {
+                return _dob;
+            }
+        }
+
+        public int Salary
+        {
+            get
+            {
+                return _salary;
+            }
+        }
+        public String Da
+        {
+            get
+            {
+                return _da;
+            }
+        }
+        public String Department
+        {
+            get
+            {
+                return _department;
+            }
+        }
+
+        public List<Employee> GetReportees()
+        {
+            return _reportees;
+        }
+
+        [NeedRefactoring]
+        public void addReportee(Employee employee)
+        {
+            _reportees.Add(employee);
+        }
+
+
+        public override String ToString()
+        {
+            return Id + " " + Name + " " + Desg + " " + Dob
+                + " " + Salary + " " + Department;
+        }
+
+    }
+
+    public class NeedRefactoring : Attribute
+    {
+        public string Property { get; set; }
+    }
+}
---
+++ DotNET/C#/AttributeFinderApp/AttributeFinderApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace AttributeFinderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type employee = typeof(Employee);
+            MethodInfo[] methodinfo = employee.GetMethods();
+            PropertyInfo[] propertyinfo = employee.GetProperties();
+
+            Console.WriteLine("Property having Custom Attribute ");
+            foreach (PropertyInfo propinfo in propertyinfo)
+            {
+                object[] attrs = propinfo.GetCustomAttributes(true);
+                foreach (object attr in attrs)
+                {
+                    Console.WriteLine(propinfo.Name);
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Method having Custom Attribute ");
+            foreach (MethodInfo info in methodinfo)
+            {
+                object[] attrs = info.GetCustomAttributes(true);
+                foreach (object attr in attrs)
+                {
+                    Console.WriteLine(info.Name);
+                }
+            }
+
+        }
+    }
+}
