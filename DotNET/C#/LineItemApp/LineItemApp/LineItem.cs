4c3dfd851decc1109a8812125635dae16d870e0e
Brijesh <brijeshyadav152@outlook.com>
2018-02-02 17:50:34 +0530

    Added LineItem App

---
+++ DotNET/C#/LineItemApp/LineItemApp/LineItem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LineItemApp
+{
+    class LineItem
+    {
+        private int _id;
+        private String _product;
+        private int _quantity;
+        private double _unitprice;
+
+        public LineItem(int id, String product, int quantity, double unitprice)
+        {
+            _id = id;
+            _product = product;
+            _quantity = quantity;
+            _unitprice = unitprice;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public String Product
+        {
+            get
+            {
+                return _product;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return _quantity;
+            }
+        }
+
+        public double UnitPrice
+        {
+            get
+            {
+                return _unitprice;
+            }
+        }
+
+        public double CalculateTotal()
+        {
+            return Quantity * UnitPrice;
+        }
+
+        public override string ToString()
+        {
+            return Id + " " + Product + " " + Quantity + " " + UnitPrice;
+        }
+    }
+}
---
+++ DotNET/C#/LineItemApp/LineItemApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LineItemApp
+{
+    class Program
+    {
+        static HashSet<LineItem> items = new HashSet<LineItem>();
+        static void Main(string[] args)
+        {
+            caseStudyOne();
+
+            //caseStudyTwo();
+        }
+
+        private static void caseStudyTwo()
+        {
+            items.Add(new LineItem(1, "Product1", 5, 10));
+            items.Add(new LineItem(2, "Product2", 8, 12));
+            items.Add(new LineItem(3, "Product3", 10, 15));
+        }
+
+        private static void caseStudyOne()
+        {
+            List<LineItem> items = new List<LineItem>();
+            items.Add(new LineItem(1, "Product1", 5, 10));
+            items.Add(new LineItem(2, "Product2", 8, 12));
+            items.Add(new LineItem(3, "Product3", 10, 15));
+
+            foreach (LineItem item in items)
+            {
+                Console.WriteLine("Total Price of " + item.Product + "is:" + item.CalculateTotal());
+            }
+        }
+
+        public static HashSet<LineItem> Get()
+        {
+            caseStudyTwo();
+            return items;
+        }
+    }
+}
